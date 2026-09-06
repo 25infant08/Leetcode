4057227a7c0905fb71ff4ec0ccc056b339d8372a
Infant Praveenkumar V <infantpraveenkumarv@gmail.com>
2026-09-06 20:25:28 +0530

    Time: 808 ms (26.93%) | Memory: 55.1 MB (13.85%) - LeetSync

--- 1953-finding-mk-average/finding-mk-average.py
+++ 1953-finding-mk-average/finding-mk-average.py
@@ -1,7 +1,5 @@
 from collections import deque
-
 class MKAverage:
-
     def __init__(self, m: int, k: int):
         self.m = m
         self.k = k
@@ -10,56 +8,43 @@ class MKAverage:
         self.n = 100001
         self.count = [0] * (self.n + 1)
         self.sum = [0] * (self.n + 1)
-
     def update(self, bit, i, delta):
         while i <= self.n:
             bit[i] += delta
             i += i & -i
-
     def query(self, bit, i):
         res = 0
         while i:
             res += bit[i]
             i -= i & -i
         return res
-
     def kth(self, k):
         idx = 0
         bit_mask = 1 << 17
-
         while bit_mask:
             nxt = idx + bit_mask
             if nxt <= self.n and self.count[nxt] < k:
                 idx = nxt
                 k -= self.count[nxt]
             bit_mask >>= 1
-
         return idx + 1
-
     def sum_smallest(self, k):
         if k == 0:
             return 0
-
         x = self.kth(k)
         cnt_before = self.query(self.count, x - 1)
         sum_before = self.query(self.sum, x - 1)
-
         return sum_before + (k - cnt_before) * x
-
     def addElement(self, num: int) -> None:
         self.q.append(num)
         self.total += num
-
         self.update(self.count, num, 1)
         self.update(self.sum, num, num)
-
         if len(self.q) > self.m:
             old = self.q.popleft()
             self.total -= old
-
             self.update(self.count, old, -1)
             self.update(self.sum, old, -old)
-
     def calculateMKAverage(self) -> int:
         if len(self.q) < self.m:
             return -1
