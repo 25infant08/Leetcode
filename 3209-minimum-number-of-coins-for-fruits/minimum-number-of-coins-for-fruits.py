924854986bd7bee2326fae77ec49f322ef92633f
Infant Praveenkumar V <infantpraveenkumarv@gmail.com>
2026-09-15 17:05:13 +0530

    Time: 199 ms (46.08%) | Memory: 19.4 MB (77.94%) - LeetSync

---
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def minimumCoins(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        for i in range(n):
+            for j in range(i + 1, min(n, 2 * i + 2) + 1):
+                dp[j] = min(dp[j], dp[i] + prices[i])
+        return dp[n]
